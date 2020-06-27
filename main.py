991a0511bc11a8d038b11eafbb8ddc5462e3298c
evannemetz <evannemetz@gmail.com>
2020-06-27 12:34:35 -0400

    Maze file turned into 2d array and start and end points created.

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
-#Project 1
+# Project 1
+import random
+
 print('Test4')
+
+
+# reads maze files and converts them to a 2d array
+def create_grid():
+    file = open("maze1.txt")
+    lines = [line.split() for line in file]
+    return lines
+
+
+# picks the starting node, recursively calls until it finds an unblocked cell
+def point_picker(arr):
+    start_row = random.randint(0, len(arr))
+    start_col = random.randint(0, len(arr[0]))
+
+    goal_row = random.randint(0, len(arr))
+    goal_col = random.randint(0, len(arr[0]))
+
+    print(arr[start_row][start_col])
+    if arr[start_row][start_col] and arr[goal_row][goal_col] == '1':
+        point_picker(arr)
+
+    print(start_row, start_col)
+    print(goal_row, goal_col)
+
+
+if __name__ == "__main__":
+    point_picker(create_grid())
