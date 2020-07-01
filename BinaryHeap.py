6a3ed26d5a1147c209e919d215c2e56dc64b07d3
Eneshana <eneshanaltunlu@gmail.com>
2020-07-01 18:35:17 -0400

    Implemented Tie Break at the insert function

--- BinaryHeap.py
+++ BinaryHeap.py
@@ -10,19 +10,26 @@ class BinaryHeap:
     def size(self):
         return len(self.heap)
 
-    #given index, returns the indexes parent
+    # given index, returns the indexes parent
     def parent(self, i):
         return (i - 1) // 2
 
     # given index, returns the indexes left child
     def left_child(self, i):
         return 2 * i + 1
+
     # given index, returns the indexes right child
     def right_child(self, i):
         return 2 * i + 2
 
     def get(self, i):
         return self.heap[i].f
+
+    def get_x(self, i):
+        return self.heap[i].x
+
+    def get_h(self, i):
+        return self.heap[i].h
 
     # returns the root node
     def get_min(self):
@@ -40,7 +47,6 @@ class BinaryHeap:
         self.min_heapify(0)
         return smallest
 
-    # after extracting root node, it arranges the remaining nodes
     def min_heapify(self, i):
         left_node = self.left_child(i)
         right_node = self.right_child(i)
@@ -51,9 +57,13 @@ class BinaryHeap:
             largest = i
         if right_node <= self.size() - 1 and self.get(right_node) < self.get(smallest):
             smallest = right_node
+
         if smallest != i:
             self.swap(smallest, i)
             self.min_heapify(smallest)
+
+
+
 
     # pretty straightforward
     def swap(self, i, j):
@@ -63,12 +73,40 @@ class BinaryHeap:
     def insert(self, key):
         index = self.size()
         self.heap.append(key)
-
         while (index != 0):
             p = self.parent(index)
-            if self.get(p) > self.get(index):
-                self.swap(p, index)
-            index = p
+            # if the f values doesn't equal to each other insert normally
+            if self.get(p) != self.get(index):
+
+                if self.get(p) > self.get(index):
+                    print("check 1")
+                    self.swap(p, index)
+                index = p
+            # if the f values are equal,
+            elif self.get(p) == self.get(index):
+                # if the h values are not equal,
+                if self.get_h(p) != self.get_h(index):
+                    # swap based on h values
+                    if self.get_h(p) > self.get_h(index):
+                        print("check 2")
+                        self.swap(p, index)
+                    index = p
+                # if the h values are equal,
+                elif self.get_h(p) == self.get_h(index):
+                    # check if the x values are not equal
+                    if self.get_x(p) != self.get_x(index):
+                        # swap based on x values
+                        if self.get_x(p) > self.get_x(index):
+                            print("check 3")
+                            self.swap(p, index)
+                        index = p
+                        # if all f,x,h values are equal then first come first serve basis on f value
+                    elif self.get_x(p) == self.get_x(index):
+                        if self.get(p) > self.get(index):
+                            print("check 4")
+                            self.swap(p, index)
+                        index = p
+
     # prints the whole list but in binary heap way instead of straight list order.
     def print(self):
         for i in range(0, (len(self.heap) // 2)):
